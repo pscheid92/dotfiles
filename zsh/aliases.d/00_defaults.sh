7fc39a0aafa0887c4709a50df1d441febcb0e210
Patrick Scheid <p.scheid92@gmail.com>
2017-03-19 11:08:42 +0100

    Replace rm -i alias with rm -I and update file format

--- zsh/aliases.d/00_defaults.sh
+++ zsh/aliases.d/00_defaults.sh
@@ -1,14 +1,14 @@
 # Defaults
-alias cp="cp -i"		  # confirm before overwriting
-alias df="df -h"		  # use human-readable sizes
+alias cp="cp -i"	# confirm before overwriting
+alias df="df -h"	# use human-readable sizes
 alias free="free -m"	# show sizes in MB
-alias ll='ls -lh'		  # list lines with human-readable sizes
-alias la='ls -ah'		  # list all with human-readable sizes
-alias lla='ls -lah'		# list lines all with human-readable sizes
-alias rm='rm -i'      # Make rm save
+alias ll='ls -lh'	# list lines with human-readable sizes
+alias la='ls -ah' 	# list all with human-readable sizes
+alias lla='ls -lah'	# list lines all with human-readable sizes
+alias rm='rm -I'	# Make rm save
 
 # Set terminal coloring
-alias ls='ls --color=auto'	     # Coloring ls
+alias ls='ls --color=auto'	 # Coloring ls
 alias grep='grep --color=auto' 	 # Coloring grep
 alias egrep='egrep --color=auto' # Coloring egrep
 alias fgrep='fgrep --color=auto' # Coloring fgrep
