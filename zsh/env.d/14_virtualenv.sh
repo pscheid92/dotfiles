16d254b4bf327703a225f7b38223cf72b76c680d
Patrick Scheid <p.scheid92@gmail.com>
2017-12-14 21:10:44 +0100

    New virtualenvwrapper script localtion

--- zsh/env.d/14_virtualenv.sh
+++ zsh/env.d/14_virtualenv.sh
@@ -1,4 +1,4 @@
 # Setting up virtualenv
 export WORKON_HOME=~/.virtualpy
 export VIRTUALENVWRAPPER_PYTHON=`which python3`
-source /usr/bin/virtualenvwrapper_lazy.sh
+source /usr/local/bin/virtualenvwrapper_lazy.sh
