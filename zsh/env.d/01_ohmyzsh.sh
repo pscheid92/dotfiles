57ef5f8ac956ef4dde9f6a3e0d38d9f4487c08e9
Patrick Scheid <p.scheid92@gmail.com>
2024-04-18 11:12:32 +0200

    cleanup

--- zsh/env.d/01_ohmyzsh.sh
+++ zsh/env.d/01_ohmyzsh.sh
@@ -1,7 +1,7 @@
 ## Activate oh-my-zsh
 
 # Prepare
-ZSH='$HOME/.oh-my-zsh/'
+ZSH="$HOME/.oh-my-zsh/"
 DISABLE_AUTO_UPDATE='true'
 ZSH_CACHE_DIR="$HOME/.oh-my-zsh/cache-dir"
 if [[ ! -d $ZSH_CACHE_DIR ]]; then
--- zsh/func.d/shrinkpdf.sh
+++
@@ -1,5 +0,0 @@
-shrinkpdf() {
-     OUTPUT="${2:-output.pdf}"
-     \gs -sDEVICE=pdfwrite -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$OUTPUT" "$1"
-}
-
